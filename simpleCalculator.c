64a6fe51966e5d0250e2fc1eb8ba06bbb031981e
BIGTOST <alcardoso18m@gmail.com>
2025-04-25 01:39:58 +0100

    code re-organization

--- simpleCalculator.c
+++ simpleCalculator.c
@@ -2,13 +2,13 @@
 //teste
 //Function Declaration
 void printStartMenu(void);
-
+void calculatorLogic(int selector);
 
 
 int main()
 {
 	//i could use the "isFinished has the operationSelector but i choose separete the "
-	int isFinished = 1, operationSelector = 0,firstValue =0, secondValue=0;
+	int isFinished = 1, operationSelector = 0;
 	// programer runner
 	do{
 
@@ -16,52 +16,7 @@ int main()
 		scanf("%d",&operationSelector);
 		if(operationSelector != 0)
 		{
-			switch(operationSelector){
-				case 1:
-				{
-					printf("Insert the augent(first value):");
-					scanf("%d",&firstValue);
-					printf("Insert the addend(second value):");
-					scanf("%d", &secondValue);
-					printf("The result of the Addition  of the values %d + %d is %d.\n", firstValue, secondValue,firstValue+secondValue);
-				}break;
-				case 2:
-				{
-					printf("Insert the minuend(first value):");
-					scanf("%d",&firstValue);
-					printf("Insert the subtrahend(second value):");
-					scanf("%d", &secondValue);
-					printf("The result of the subtraction of the values %d - %d is %d.\n", firstValue, secondValue,firstValue-secondValue);
-				}break;
-				case 3:
-				{
-					printf("Insert multiplier(first value):");
-					scanf("%d",&firstValue);
-					printf("Insert the multiplicand(second value):");
-					scanf("%d", &secondValue);
-					printf("The result of the Multiplication of the values %d * %d is %d.\n", firstValue, secondValue,firstValue*secondValue);
-				}
-				break;
-				case 4 :
-				{
-					printf("Insert the dividend(first value):");
-					scanf("%d",&firstValue);
-
-					do
-					{
-						printf("Insert the divisor(second value):");
-						scanf("%d", &secondValue);
-						if(secondValue == 0)
-							printf("The divisor cannot be 0.");
-					}while(secondValue == 0);
-
-					printf("The result of the division  of the values %d / %d is %d.\n", firstValue, secondValue,firstValue/secondValue);
-				}break;
-				default:
-				{
-					printf("Selected number Unknown");
-				}break;
-			}
+			calculatorLogic(operationSelector);
 		}
 		else
 		{
@@ -84,5 +39,59 @@ void printStartMenu()
 	printf("\tEnd Program        \t   [0]\n");
 }
 
+void calculatorLogic(int selector)
+{
+	int firstValue   =0,
+	    secondValue = 0;
 
+	switch(selector){
+		case 1:
+		{
+			printf("Insert the augent(first value):");
+			scanf("%d",&firstValue);
+			printf("Insert the addend(second value):");
+			scanf("%d", &secondValue);
+			printf("The result of the Addition  of the values %d + %d is %d.\n", firstValue, secondValue,firstValue+secondValue);
+		}
+		break;
+		case 2:
+		{
+			printf("Insert the minuend(first value):");
+			scanf("%d",&firstValue);
+			printf("Insert the subtrahend(second value):");
+			scanf("%d", &secondValue);
+			printf("The result of the subtraction of the values %d - %d is %d.\n", firstValue, secondValue,firstValue-secondValue);
+		}
+		break;
+		case 3:
+		{
+			printf("Insert multiplier(first value):");
+			scanf("%d",&firstValue);
+			printf("Insert the multiplicand(second value):");
+			scanf("%d", &secondValue);
+			printf("The result of the Multiplication of the values %d * %d is %d.\n", firstValue, secondValue,firstValue*secondValue);
+		}
+		break;
+		case 4 :
+		{
+			printf("Insert the dividend(first value):");
+			scanf("%d",&firstValue);
+
+			do
+			{
+				printf("Insert the divisor(second value):");
+				scanf("%d", &secondValue);
+				if(secondValue == 0)
+					printf("The divisor cannot be 0.");
+			}while(secondValue == 0);
+
+			printf("The result of the division  of the values %d / %d is %d.\n", firstValue, secondValue,firstValue/secondValue);
+		}
+		break;
+		default:
+		{
+			printf("Selected number Unknown");
+		}break;
+	}
+}
 
